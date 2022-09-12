fe5eddce486c321c44c9a24d180b6d85289d0bf5
mamiruka <pyarka.sleep@yandex.ru>
2022-09-12 23:43:11 +0800

    добавлено отображение файлов

--- src/components/LeftPanel/LeftPanel.tsx
+++ src/components/LeftPanel/LeftPanel.tsx
@@ -1,39 +1,14 @@
-import React, { useCallback, useState } from "react";
-import { FileLoader, LeftPanelContainer } from "./LeftPanelStyles";
-import { useDropzone } from 'react-dropzone';
+import React from "react";
+import { LeftPanelContainer } from "./LeftPanelStyles";
+import FileLoader from "./components/FileLoader/FileLoader";
 
 const LeftPanel = () => {
-    const [files, setFiles] = useState<File[]>([]);
-    const reader = new FileReader();
 
-    const onDrop = useCallback((newFiles: Array<File>) => {
-        let oldFiles = files;
-        newFiles.forEach((newFile: File) => {
-            oldFiles.push(newFile)
-        })
-        setFiles(oldFiles);
-    }, [files]);
-
-    const {
-        getRootProps,
-        getInputProps
-    } = useDropzone({
-        onDrop
-    });
-
-    const renderFiles = () => {
-        const filesList = files.map((file, i) => <div key={i}>{file.name}</div>)
-        return <>{filesList}</>;
-    }
 
     return (
         <LeftPanelContainer>
             Left Panel Container
-            <div {...getRootProps()}>
-                <input {...getInputProps()} />
-                <div>Drag and drop your images here.</div>
-            </div>
-            {renderFiles()}
+            {FileLoader()}
         </LeftPanelContainer>
     )
 }
--- src/components/LeftPanel/LeftPanelStyles.tsx
+++ src/components/LeftPanel/LeftPanelStyles.tsx
@@ -7,11 +7,3 @@ export const LeftPanelContainer = styled.div`
     border-right: solid grey 1px;
 `;
 
-export const FileLoader = styled.label`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    height: 10vh;
-    border: ${({ isDragging }: { isDragging: boolean }) => isDragging ? 'solid #c21d00 2px' : 'solid #00c210 1px'};
-    filter: ${({ isDragging }: { isDragging: boolean }) => isDragging ? 'saturate(30%), blur(50%)' : ''}
-`
